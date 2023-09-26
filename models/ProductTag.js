639ec67b7a059bcc696ee8dcdbb76de3002d3dc0
Adrian Patton <adrian.j.patton33@gmail.com>
2023-09-26 19:04:55 -0400

    set up models and their associations

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -5,10 +5,34 @@ const sequelize = require('../config/connection');
 class ProductTag extends Model {}
 
 ProductTag.init(
-  {
     // define columns
-  },
-  {
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      product_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'product',
+          key: 'id',
+          unique: false
+        }
+      },
+      product_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'product',
+          key: 'id',
+          unique: false
+        }
+      },
+      
+
+  
+
     sequelize,
     timestamps: false,
     freezeTableName: true,
